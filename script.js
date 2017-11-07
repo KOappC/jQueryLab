70e21d61a808426e8e4a1fa5738284a5c2523285
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-11-07 11:56:13 -0500

    completed JavaScript for first part of lab

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 $(document).ready(function () {
 
 var circles;
-var usersName;
+var userName;
+var userNumber;
+var userParty;
 var name;
-var ogText;
+var number;
+var party;
+var container;
 
 $(".circles").on("click", function(event) {
     $(".newReservation").css("display", "flex");
     circles = event;
+    container = $(this);
+    //console.log(container["0"].innerText);
     if ($(this).hasClass("taken")) {
         alert("I'm sorry, this table is reserved");
+        $(".newReservation").css("display", "none");
     }
 });
 
 $(".circles").on("mouseenter", function(event) {
-    console.log($(this).attr("name"));
     name = $(this).attr("name");
-    ogText = $(this).text(name);
+    number = $(this).attr("number");
+    party = $(this).attr("party");
+    if ($(this).hasClass("taken")) {
+        $(this).css("cursor", "not-allowed");
+    }
 
 }).on("mouseleave", function(event) {
-    console.log("Mouse left this circle");
+
 });
 
 $(".Xend").on("click", function() {
@@ -27,10 +37,14 @@ $(".Xend").on("click", function() {
 });
 
 $("#save").on("click", function(event) {
-    usersName = $("#userName").val();
-    console.log(userName);
-    circles.target.setAttribute("name", usersName);
-    $(".circles").addClass("taken");
+    userName = $("#userName").val();
+    userNumber = $("#userNumber").val();
+    userParty = $("#userParty").val();
+    circles.target.setAttribute("name", userName);
+    circles.target.setAttribute("number", userNumber);
+    circles.target.setAttribute("party", userParty);
+    container.addClass("taken");
+    container.css("background-color", "#ADAAAA").css("color", "#CCCCCC");
     $(".newReservation").css("display", "none");
 });
 
@@ -40,18 +54,18 @@ $("#save").on("click", function(event) {
 
     Objectives:
 
-    1. Displays the restaurant’s floor plan.
-    2. Visually notifies the user that seats are available or not.
-    3. Displays a form when an open table is clicked on.
-    4. Hides the form when the user submits the form.
+    //DONE  1. Displays the restaurant’s floor plan.
+    //DONE  2. Visually notifies the user that seats are available or not.
+    //DONE  3. Displays a form when an open table is clicked on.
+    //DONE  4. Hides the form when the user submits the form.
 
     Build specs:
 
-    1. Each table must have either an “available” or a “reserved” class.
-    2. The form must remain hidden until a user clicks on a table with the class “available”.
+    //DONE  1. Each table must have either an “available” or a “reserved” class.
+    //DONE  2. The form must remain hidden until a user clicks on a table with the class “available”.
     //DONE  3. There should be a way for the user to exit the form without submitting.
     //DONE  4. When the user submits the form, the form is hidden and the class of the selected seat is changed from “available” to “reserved”.
-    5. Change the cursor property to “not-allowed” if the table has a “reserved” class.
-    6. Changes the appearance of an open table when the mouse moves over the table, as well as changes the cursor.
+    //DONE  5. Change the cursor property to “not-allowed” if the table has a “reserved” class.
+    //DONE  6. Changes the appearance of an open table when the mouse moves over the table, as well as changes the cursor.
 
 */
