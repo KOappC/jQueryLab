eb48fdb493506b976fc95ddb5c88a5ec1711549c
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-11-07 10:28:24 -0500

    storing name from form, changing saved table to 'taken' class

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 $(document).ready(function () {
 
-var saveState = false;
+var circles;
+var usersName;
+var name;
+var ogText;
 
-$(".circles").on("click", function() {
+$(".circles").on("click", function(event) {
     $(".newReservation").css("display", "flex");
-    if (saveState === true) {
+    circles = event;
+    if ($(this).hasClass("taken")) {
         alert("I'm sorry, this table is reserved");
     }
+});
+
+$(".circles").on("mouseenter", function(event) {
+    console.log($(this).attr("name"));
+    name = $(this).attr("name");
+    ogText = $(this).text(name);
+
+}).on("mouseleave", function(event) {
+    console.log("Mouse left this circle");
 });
 
 $(".Xend").on("click", function() {
    $(".newReservation").css("display", "none");
 });
 
-$("#save").on("click", function() {
-    
-})
+$("#save").on("click", function(event) {
+    usersName = $("#userName").val();
+    console.log(userName);
+    circles.target.setAttribute("name", usersName);
+    $(".circles").addClass("taken");
+    $(".newReservation").css("display", "none");
+});
 
 }); // Closing of document
 
@@ -33,7 +50,7 @@ $("#save").on("click", function() {
     1. Each table must have either an “available” or a “reserved” class.
     2. The form must remain hidden until a user clicks on a table with the class “available”.
     //DONE  3. There should be a way for the user to exit the form without submitting.
-    4. When the user submits the form, the form is hidden and the class of the selected seat is changed from “available” to “reserved”.
+    //DONE  4. When the user submits the form, the form is hidden and the class of the selected seat is changed from “available” to “reserved”.
     5. Change the cursor property to “not-allowed” if the table has a “reserved” class.
     6. Changes the appearance of an open table when the mouse moves over the table, as well as changes the cursor.
 
